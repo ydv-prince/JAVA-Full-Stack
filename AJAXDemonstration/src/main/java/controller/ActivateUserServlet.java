688811a6a47c7aec67e81a74744fd9dc658f02df
ydv-prince <psychoaimerhere@gmail.com>
2026-01-21 10:26:31 +0530

    User Activation page

--- AJAXDemonstration/src/main/java/controller/ActivateUserServlet.java
+++ AJAXDemonstration/src/main/java/controller/ActivateUserServlet.java
@@ -5,39 +5,34 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Set;
 
-/**
- * Servlet implementation class ActivateUserServlet
- */
 @WebServlet("/ActivateUserServlet")
 public class ActivateUserServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
+    private static final long serialVersionUID = 1L;
+
     public ActivateUserServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
         PrintWriter pw = response.getWriter();
-        Set<String> users = (Set<String>)getServletContext().getAttribute("loggedUser");
-        if(users == null || users.isEmpty()) {
-        	pw.println("No active user");
-        }else {
-        	for(String u : users) {
-        		pw.print("<li>"+u+"</li>");
-        	}
+
+        Set<String> users = (Set<String>) getServletContext().getAttribute("loggedUser");
+
+        if (users == null || users.isEmpty()) {
+            pw.print("No active users");
+        } else {
+            for (String u : users) {
+                pw.print("<li>" + u + "</li>");
+            }
         }
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
+    }
 }
