cf5972ccf1d642bca4214ca306a51ae7a747fcbd
ydv-prince <psychoaimerhere@gmail.com>
2026-01-21 10:27:00 +0530

    Login Servlet page

--- AJAXDemonstration/src/main/java/controller/LoginServlet.java
+++ AJAXDemonstration/src/main/java/controller/LoginServlet.java
@@ -7,42 +7,60 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
-import java.util.Set;
+import java.io.PrintWriter;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
     public LoginServlet() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        String username = request.getParameter("user");
-        String password = request.getParameter("pwd");
+        // TODO Auto-generated method stub
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+
+        String username = request.getParameter("uname");
+        String pwd = request.getParameter("pwd");
 
         if (username != null && !username.isEmpty()) {
+
             HttpSession session = request.getSession();
             session.setAttribute("user", username);
 
             ServletContext context = getServletContext();
+
             synchronized (context) {
                 Set<String> users = (Set<String>) context.getAttribute("loggedUser");
+
                 if (users == null) {
-                    users = new HashSet<>();
-                }
-                users.add(username);
+                    users = new HashSet<>(); // create collection
+                } // inner if
+
+                users.add(username); // take data from user input
                 context.setAttribute("loggedUser", users);
-            }
+            } // end of synchronized
+
             response.sendRedirect("userHome.jsp");
-        } else {
+
+        } // outer if
+        else {
             response.sendRedirect("login.jsp");
         }
     }
