967f671d35ca11a3b46b9df2883b9e8b8e7204ff
ydv-prince <psychoaimerhere@gmail.com>
2026-01-30 12:47:18 +0530

    Controller page for Home JSP

--- SpringMVCMaven1/src/main/java/com/prince/Controller/HomeController.java
+++ SpringMVCMaven1/src/main/java/com/prince/Controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.prince.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 	
 	@GetMapping("/")
