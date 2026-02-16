b4fe9207eab6b86705a6a3639e9275ce3b517a17
ydv-prince <psychoaimerhere@gmail.com>
2026-02-16 14:49:45 +0530

    User DAO

---
+++ Notes_App/src/main/java/com/maven/dao/UserDAO.java
@@ -0,0 +1,45 @@
+package com.maven.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.maven.model.User;
+import com.maven.util.DBConnection;
+
+public class UserDAO {
+
+    public boolean register(User user) {
+        boolean status = false;
+        try (Connection con = DBConnection.getConnection()) {
+            String sql = "INSERT INTO users(username,password) VALUES(?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.executeUpdate();
+            status = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    public User login(String username, String password) {
+        User user = null;
+        try (Connection con = DBConnection.getConnection()) {
+            String sql = "SELECT * FROM users WHERE username=? AND password=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
