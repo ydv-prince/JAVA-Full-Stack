b14060f46b393d28fdf22567bad9c9e3701a585d
Prince Kumar <psychoaimerhere@gmail.com>
2026-02-28 11:36:37 +0530

    User class with methods

---
+++ spring_gmail_service/src/main/java/com/gmail/model/User.java
@@ -0,0 +1,17 @@
+package com.gmail.model;
+public class User {
+private String name;
+private String email;
+public void setName(String name) {
+	this.name=name;
+}
+public void setEmail(String email) {
+	this.email=email;
+}
+public String getName() {
+	return name;
+}
+public String getEmail() {
+	return email;
+}
+}
